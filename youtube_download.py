a1154af981298b8b3b02c7f39d9bd5b9d3aee906
WU PO-CHI <po-chi@HUNGdeMacBook-Pro.local>
2023-07-28 21:19:05 +0800

    optimize download class

--- youtube_download.py
+++ youtube_download.py
@@ -6,25 +6,23 @@ from urllib.parse import parse_qs, urlparse
 from pytube import YouTube
 from pydub import AudioSegment
 from webvtt import WebVTT
-
-# import whisper
-# import subprocess
-# import threading
+import sqlite3
+from datetime import datetime
 
 # Load environment variables from .env file
 load_dotenv()
 # Access API key from environment variable
 openai.api_key = os.environ.get('GPT_API_KEY')
-# print(openai.api_key)
 
 class YoutubeDownload:
     def __init__(self, yt_url):
-        self.youtube_url = yt_url
-        self.file_name = None  # Initialize file_name as None
+        self.yt_url = yt_url
+        self.video_id = self.extract_video_id(yt_url)
+        self.yt = YouTube(f'https://www.youtube.com/watch?v={self.video_id}')
         self.current_folder = os.getcwd()
-        # self.youtube_url = 'https://www.youtube.com/watch?v=v8jnUQAXHaw&ab_channel=%E8%B2%A1%E5%A0%B1%E7%8B%97'
-        # self.file_name = '財報狗_242'
-    
+        self.conn = sqlite3.connect('./sqlite/infosift.db')
+        self.cursor = self.conn.cursor()
+
     def extract_video_id(self, url):
         query = urlparse(url)
         if query.hostname == 'youtu.be': # Shortened URL
@@ -37,49 +35,40 @@ class YoutubeDownload:
             elif query.path[:3] == '/v/': # Old-style URL
                 return query.path.split('/')[2]
         # None of the above cases match, so raise an exception
-        raise ValueError(f"Invalid YouTube URL: {url}")                   
+        raise ValueError(f"Invalid YouTube URL: {url}")       
+    
+    def download(self):
+        video = self.yt.streams.get_highest_resolution()
+        video.download(output_path='./video/', filename=f'{self.video_id}.mp4')
 
-    def extract_video_info(self, youtube_url):
-        try:
-            video = YouTube(youtube_url)
+        video_info = {
+            'video_id': self.video_id,
+            'url': self.yt_url,
+            'title': self.yt.title,
+            'channel': self.yt.author,
+            'video_length': self.yt.length,
+            'video_size': video.filesize,
+            'views_count': self.yt.views,
+            'created_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }
 
-            video_length = str(video.length) + ' seconds'
-            video_size = str(round(video.streams.filter(file_extension='mp4').first().filesize*0.000001,2)) + ' MB'
-            video_title = video.title
-            channel_name = video.author
-            # channel_subscribers = video.author.subscriber_count
-            views_count = str(int(round(video.views/1000,0))) + "K views"
-
-            return {
-                "Video Title": video_title,
-                "Channel Name": channel_name,
-                "Video Length": video_length,
-                "Video Size": video_size,
-                # "Channel Subscribers": channel_subscribers,
-                "Views": views_count
-            }
-
-        except Exception as e:
-            return {"Error": str(e)}
-
-    def download_youtube_video(self, video_id):
-        youtube_url = f"https://www.youtube.com/watch?v={video_id}"
-        yt = YouTube(youtube_url, use_oauth=True, allow_oauth_cache=True)
-        
-        # Get the first available audio or video stream
-        stream = yt.streams.filter(progressive=True, file_extension='wav').first()
-        if not stream:
-            stream = yt.streams.filter(only_audio=True).first()
-        if not stream:
-            raise Exception("No available streams found.")
-        
-        # Download the stream
-        self.file_name = yt.title
-        stream.download(output_path='./video/', filename=f'{self.file_name}.mp4')
-        print(yt.title)
-        # os.rename(f'./video/{yt.title}.mp4',f'./video/{self.file_name}.mp4')
-        
-        return yt.title
+        print(video_info)
+        placeholders = ', '.join(['?'] * len(video_info))
+        columns = ', '.join(video_info.keys())
+        sql = f'''
+            INSERT INTO video ({columns})
+            VALUES ({placeholders})
+            ON CONFLICT(video_id) DO UPDATE SET
+            title = excluded.title,
+            channel = excluded.channel,
+            video_length = excluded.video_length,
+            video_size = excluded.video_size,
+            views_count = excluded.views_count,
+            updated_time = excluded.updated_time
+        '''
+        self.cursor.execute(sql, list(video_info.values()))
+        self.conn.commit()
 
     def convert_mp4_to_wav(self, mp4_path, wav_path):
         audio = AudioSegment.from_file(mp4_path, "mp4")
@@ -90,70 +79,62 @@ class YoutubeDownload:
         audio = audio.set_frame_rate(new_rate)
         audio.export(output_path, format="wav")
 
-    # Too Slow
-    def video_to_transcript(self, format='wav'):
-
-        model = whisper.load_model("large")
-        result = model.transcribe(f"./video/{self.file_name}.{format}", language='zh')
-        # print(result["text"])
-        # Export to txt
-        with open(f'./transcript/{self.file_name}.wav.txt', 'w') as f:
-            f.write(result["text"])
-
     # Money
     def whisper_api(self,format='wav'):
         audio_file= open("/path/to/file/audio.mp3", "rb")
         transcript = openai.Audio.transcribe("whisper-1", audio_file, language='zh')
         # Export to txt
-        with open(f'./transcript/{self.file_name}.{format}.txt', 'w') as f:
+        with open(f'./transcript/{self.video_id}.{format}.txt', 'w') as f:
             f.write(transcript['text'])
 
 
     def whisper_cpp(self):
-        command = f"./whisper/whisper.cpp/main -m ./whisper/whisper.cpp/models/ggml-large.bin -l zh -ovtt -f '{self.current_folder}/video/{self.file_name}.wav'"
+        command = f"./whisper/whisper.cpp/main -m ./whisper/whisper.cpp/models/ggml-large.bin -l zh -ovtt -f '{self.current_folder}/video/{self.video_id}.wav'"
         print(command)
         os.system(command)
 
-        source_file = f'./video/{self.file_name}.wav.vtt'
+        source_file = f'./video/{self.video_id}.wav.vtt'
         destination_folder = './transcript/'
         filename = os.path.basename(source_file)
         destination_file = os.path.join(destination_folder, filename)
         os.replace(source_file, destination_file)
 
-        subtitles = WebVTT().read(f'./transcript/{self.file_name}.wav.vtt')
-        with open(f'./transcript/{self.file_name}.wav.txt', 'w', encoding='utf-8') as txt_file:
+        subtitles = WebVTT().read(f'./transcript/{self.video_id}.wav.vtt')
+        with open(f'./transcript/{self.video_id}.wav.txt', 'w', encoding='utf-8') as txt_file:
             for subtitle in subtitles:
                 txt_file.write(subtitle.text + '\n')
-        print(f"Conversion successful. TXT file saved as {self.file_name}.wav.txt.")
+        print(f"Conversion successful. TXT file saved as {self.video_id}.wav.txt.")
 
     def run(self):
-        video_id = self.extract_video_id(self.youtube_url)
-        file_name = self.download_youtube_video(video_id)
-        print(f"Download Completed: {file_name}")
-        self.convert_mp4_to_wav(f'./video/{self.file_name}.mp4',f'./video/{self.file_name}.wav')
-        print(f"mp4 to wav Completed: {file_name}")
+        self.download()
+        print(f"Download Completed: {self.yt.title}")
+        self.convert_mp4_to_wav(f'./video/{self.video_id}.mp4',f'./video/{self.video_id}.wav')
+        print(f"mp4 to wav Completed: {self.yt.title}")
         rate = 16000
-        self.resample_wav(f'./video/{self.file_name}.wav',f'./video/{self.file_name}.wav', new_rate=rate)
-        print(f'Resample to {rate} Hz Completed: {file_name}')
-        # self.video_to_transcript(format='mp4')
+        self.resample_wav(f'./video/{self.video_id}.wav',f'./video/{self.video_id}.wav', new_rate=rate)
+        print(f'Resample to {rate} Hz Completed: {self.yt.title}')
         # self.whisper_api(format='mp4')
         self.whisper_cpp()
         # self.whisper_cpp_streamlit()
-        print(f'Video to transcript Completed: {self.file_name}')
+        print(f'Video to transcript Completed: {self.yt.title}')
+
+    # def run(self):
+    #     # your existing run method implementation
+
+    def __del__(self):
+        self.conn.close()
 
 def parse_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-u','--url', type=str, required=True, help='url of Youtube video')
-    parser.add_argument('-n','--file_name', type=str, required=True, help='file name')
     args = parser.parse_args()
     return args
 
-# usage: python -m youtube_download -u 'https://www.youtube.com/watch?v=ZQyMZKQessg&t=31s&ab_channel=%E8%B2%A1%E5%A0%B1%E7%8B%97' -n '財報狗_241'
+# usage: python -m youtube_download -u 'https://www.youtube.com/watch?v=ZQyMZKQessg&t=31s&ab_channel=%E8%B2%A1%E5%A0%B1%E7%8B%97'
 
 if __name__ == '__main__':
 
     args = parse_args()
     YoutubeDownload(
         yt_url = args.url,
-        # file_name = args.file_name
     ).run()
